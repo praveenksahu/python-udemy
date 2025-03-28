923843d8f91442600e475909d70b84f47c8a6802
praveenksahu <praveen.sahu@yahoo.com>
2025-03-28 19:50:22 +0530

    14th

--- Python_Selenium/Locator_Extension.py
+++ Python_Selenium/Locator_Extension.py
@@ -13,6 +13,10 @@ driver =webdriver.Chrome(service=service_obj)
 driver.get("https://rahulshettyacademy.com/client/")
 
 driver.find_element(By.LINK_TEXT,"Forgot password?").click()
-
+driver.maximize_window()
+driver.find_element(By.XPATH,"//form/div[1]/input").send_keys("demo@gmail.com")
+driver.find_element(By.CSS_SELECTOR,"form div:nth-child(2) input").send_keys("Hello@1234")
+driver.find_element(By.CSS_SELECTOR,"#confirmPassword").send_keys("Hello@1234")
+driver.find_element(By.XPATH,"//button[text()='Save New Password']").click()
 
 time.sleep(2)
